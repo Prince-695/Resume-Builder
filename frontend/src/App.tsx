cb870fc1707ea109711ab74901df5bf6f37b8d80
Prince Rathod <rathodprince411@gmail.com>
2025-05-12 18:20:14 +0530

    Page routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
+import Login from './pages/Auth/Login'
+import SignUp from './pages/Auth/SignUp'
+import LandingPage from './pages/ResumeUpdate/LandingPage'
+import Dashboard from './pages/Home/Dashboard'
+import EditResume from './pages/ResumeUpdate/Forms/EditResume'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Router>
+        <Routes>
+          {/* Default Routes */}
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/resume/:resumeId" element={<EditResume />} />
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
